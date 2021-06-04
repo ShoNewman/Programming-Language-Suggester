9a71bc767a1acdb596433cc4b1cf5c88562a6b3a
Shoshana Newman <sho.newman@gmail.com>
2021-06-04 10:44:29 -0700

    Add jQuery to show and hide different forms with each form submission

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,36 @@
 
 
 // User Interface Logic
-$()
+$(document).ready(function() {
+  // Work Style Questions
+  $('form#workStyleForm').submit(function(event) {
+    event.preventDefault();
+    $('#personality').show();
+    $('#workStyle').hide();
+  })
+
+  // Personality Questions
+  $('form#personalityForm').submit(function(event) {
+    event.preventDefault();
+    $('#spiritAnimal').show();
+    $('#personality').hide();
+  })
+
+  // Spirit Animal Questions
+  $('form#spiritAnimalForm').submit(function(event) {
+    event.preventDefault();
+    $('#personalInfo').show();
+    $('#spiritAnimal').hide();
+  })
+
+  // Personal Info Questions
+  $('form#personalInfoForm').submit(function(event) {
+    event.preventDefault();
+    $('#results').show();
+    $('#personalInfo').hide();
+  })
+
+  // results
+
+
+})
